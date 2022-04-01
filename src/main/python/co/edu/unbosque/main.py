b94975d11213c440ea30ed08ba146033efde00c0
alejo <alejo.80.123@gmail.com>
2022-03-31 22:13:37 -0500

    100%

--- src/main/python/co/edu/unbosque/main.py
+++ src/main/python/co/edu/unbosque/main.py
@@ -1,17 +1,62 @@
 import read
-import re
+import statistics as stat
 
 if '__main__' == __name__:
     print('')
 
-datos = read.readExcelTitanic(
-    url='C:/Users/alejo/IdeaProjects/bd-2 titanic/src/resource/titanic.xlsx',
+datos1 = read.readExcelTitanic(
+    url='C:/Users/alejo/IdeaProjects/bd-2 titanic/src/resource/titanic1.xlsx',
+)
+
+datos2 = read.readExcelTitanic(
+    url='C:/Users/alejo/IdeaProjects/bd-2 titanic/src/resource/titanic2.xlsx',
+)
+
+datos3 = read.readExcelTitanic(
+    url='C:/Users/alejo/IdeaProjects/bd-2 titanic/src/resource/titanic3.xlsx',
 )
 
 
 def imprimirDatos(datos):
     for i in range(len(datos)):
-        print(datos[i])
+        print('{')
+        print('   survived:', datos[i]['survived'])
+        print('   name:', datos[i]['name'])
+        print('   sex:', datos[i]['sex'])
+        print('   age:', datos[i]['age'])
+        print('}')
 
 
-imprimirDatos(datos)
+def evaluarDatos(datos):
+    try:
+        listnull = []
+        listSex = []
+        for dato in datos:
+            sex = str(dato['sex'])
+            if sex == 'nan':
+                listnull.append(dato)
+            else:
+                listSex.append(sex)
+
+        moda = stat.mode(listSex)
+
+        if len(listnull) != 0:
+            for i in listnull:
+                i['sex'] = moda
+
+        print('--------------------------------------------')
+        imprimirDatos(listnull)
+        print('La moda es =', moda)
+        print('--------------------------------------------')
+    except Exception as ex:
+        print(ex)
+
+
+# Caso normal
+evaluarDatos(datos1)
+
+# Caso faltan algunos
+evaluarDatos(datos2)
+
+# Caso falta uno
+evaluarDatos(datos3)
